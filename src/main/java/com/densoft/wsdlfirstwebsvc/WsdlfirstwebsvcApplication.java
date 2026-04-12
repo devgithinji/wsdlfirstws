ca5d0ff35669111df685487ca965b432a7bfb555
dennis githinji <wakahiad@gmail.com>
2026-04-13 00:01:34 +0300

    create the WSDL

---
+++ src/main/java/com/densoft/wsdlfirstwebsvc/WsdlfirstwebsvcApplication.java
@@ -0,0 +1,13 @@
+package com.densoft.wsdlfirstwebsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlfirstwebsvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdlfirstwebsvcApplication.class, args);
+    }
+
+}
